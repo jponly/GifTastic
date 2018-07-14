b84c9c600634735a233ed96621a041bfcdeafa3c
jponly <jpangeli514@gmail.com>
2018-07-13 19:42:49 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,61 @@
 $(document).ready(function () {
 
-  var simpsons = [];
 
+
+
+    var simpsons = [];
 
 
       function renderButtons() {
         $("#simpsons_view").empty();
 
-          for (var i = 0; i < simpsons.length; i++) {
-            var a = $('<button>');
-            a.addClass("simpson");
-            a.attr("data-name", simpsons[i]);
-            a.text(simpsons[i]);
-            $("#simpsons_view").append(a);
+        for (var i = 0; i < simpsons.length; i++) {
+          var a = $('<button>');
+          a.addClass("simpson");
+          a.attr("data-name", simpsons[i]);
+          a.text(simpsons[i]);
+          $("#simpsons_view").append(a);
 
         }
 
       }
 
-        $("#add-simpson").on("click", function (event) {
-          event.preventDefault();
-          var simpson = $("#simpson-input").val().trim();
-          simpsons.push(simpson);
+      $("#add-simpson").on("click", function (event) {
+        event.preventDefault();
+        var simpson = $("#simpson-input").val().trim();
+        simpsons.push(simpson);
 
-          renderButtons();
-  });
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=aoSPXoHUbFoGrtwGZUsQSBL735BHRriI&tag=simpsons";
 
-          renderButtons();
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+
+
+          .then(function (response) {
+
+            var imageUrl = response.data.image_original_url;
+            var simpImage = $("<img>");
+            simpImage.attr("src", imageUrl);
+            simpImage.attr("alt", "simpson image");
+            $("#simpsons_view").append(simpImage);
+
+
+
+          });
+
+
+
+        renderButtons();
+
+      });
+
+        renderButtons();
+
+
+
 
 }); // document ready
 
@@ -35,33 +64,18 @@ $(document).ready(function () {
 
 
 
-// ======== possibilities --------//
-
-// $("#simpsons_btn").on("click", function(event) {
-
-//   event.preventDefault();
-
-//   // var simpson = $("#simpson-input").val();
-
-//   var queryURL = "https://api.giphy.com/v1/gifs/search/the-simpsons?api_key=aoSPXoHUbFoGrtwGZUsQSBL735BHRriI&limit=20";
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-//     //console.log(response.data[0].bitly_url);
-
-//     // OPTION 1
-//     $("#gif_box").append("<img src='" + response.data[7].images.original.url + "' />");
-
-//     // OPTION 2
-//     // var img = $("<img/>");
-//     // img.attr("src", response.data[0].images.original.url);
-//     // $("body").append(img);
 
 
-//   });
 
-// });
+
+
+
+
+
+
+
+
+
+
+
 
