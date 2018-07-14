d010495eedaa50accfad9a3f6174cf697cb38c5e
jponly <jpangeli514@gmail.com>
2018-07-14 14:33:15 -0700

    updatejul14

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,9 +5,7 @@ $(document).ready(function () {
   var simpsons = ["Homer", "Marge", "Bart", "Lisa", "Maggie"];
   console.log(simpsons);
 
-
-
-  // function renderButtons() {
+  function renderButtons() {
     $("#simpsons_view").empty();
 
     for (var i = 0; i < simpsons.length; i++) {
@@ -16,74 +14,49 @@ $(document).ready(function () {
       a.attr("data-name", simpsons[i]);
       a.text(simpsons[i]);
       $("#simpsons_view").append(a);
-
-
     }
-
-  // }
+  }
+  renderButtons();
 
   $("#simpsons_view").on("click", "button", function (event) {
-    // event.preventDefault();
+    //  event.preventDefault();
     var simpson = $(this).text().trim();
     // simpsons.push(simpson);
     console.log(simpson);
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aoSPXoHUbFoGrtwGZUsQSBL735BHRriI&q=" + simpson + "&limit=10";
 
-
     $.ajax({
       url: queryURL,
       method: "GET"
-    })
+    }).then(function (response) {
+      console.log(response);
 
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
 
-      .then(function (response) {
-        console.log(response);
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          var gifDiv = $("<div class='item'>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+          gifDiv.append(p);
+          gifDiv.append(personImage);
+          $("body").append(gifDiv);
 
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            var gifDiv = $("<div class='item'>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-            gifDiv.append(p);
-            gifDiv.append(personImage);
-            $("body").append(gifDiv);
-          
-          }
         }
-
-        
-
-        // var imageUrl = response.data.image_original_url;
-        // var simpImage = $("<img>");
-        // simpImage.attr("src", imageUrl);
-        // simpImage.attr("alt", "simpson image");
-        // $("#simpsons_view").append(simpImage);
-
-          
-
-
-
-
-      });
-
-
-
-    // renderButtons();
-
+      }
+    });
   });
 
-  // renderButtons();
-
-
-
-
-
-
+  $("#add-simpson").on("click", function(event) {
+    // event.preventDefault();
+    var simpson = $("#simpson-input").val().trim();
+    simpsons.push(simpson);
+    console.log(simpsons);
+    renderButtons();
+  });
 
 }); // document ready
 
